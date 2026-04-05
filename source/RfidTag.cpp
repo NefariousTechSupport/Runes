33335803c4ccea18f9259fc0953948fa4ca42739
NefariousTechSupport <64808609+NefariousTechSupport@users.noreply.github.com>
2026-04-05 19:05:49 +0100

    Rewrite Runes::RfidTag::SaveToFile to be less error prone
    also fixes a crash when it fails to open the file

--- source/RfidTag.cpp
+++ source/RfidTag.cpp
@@ -71,21 +71,19 @@ bool Runes::RfidTag::SaveToFile(const char* path)
 {
 	FILE* f = fopen(path, "wb");
 	int res = 0;
+	bool success = true;
 
-	if(!f) goto error;
+	if (f)
+	{
+		fseek(f, 0, SEEK_SET);
+		res = fwrite(this->_tag, 0x01, 1024, f);
 
-	fseek(f, 0, SEEK_SET);
-	res = fwrite(this->_tag, 0x01, 1024, f);
+		success = (res == 1024);
 
-	if(res != 1024) goto error;
+		fclose(f);
+	}
 
-	fflush(f);
-	fclose(f);
-	return true;
-
-	error:
-	fclose(f);
-	return false;
+	return success;
 }
 
 
